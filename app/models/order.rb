c37cc97a369a2e0c263d1657607f736c7f940df9
changrh <changruohan@gmail.com>
2014-09-10 23:37:46 +0800

    Change in order model (purely syntax)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ActiveRecord::Base
     belongs_to :dish
+    validates :name, presence: true
+    validates :phone_num, presence: true
     validates_numericality_of :phone_num
-    validates_presence_of :phone_num
     def validate
       if dish.orders.size >= dish.stock
         errors.add_to_base("The dish has been sold out !")
